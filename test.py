5fb02e713e33f6df1fad4c6a45720df33ed95dbf
jacob <jacob@a827fe29-2235-0410-a7a9-cacdfe24a5d3>
2007-12-25 19:58:03 +0000

    - Add templates directory - test.py fixes: rename /tp to /asyncpagetest, add -prof flag to enable/disable profiler
    git-svn-id: http://chiral.j4cbo.com/svn/trunk@82 a827fe29-2235-0410-a7a9-cacdfe24a5d3

--- test.py
+++ test.py
@@ -2,6 +2,8 @@
 
 print "Loading modules..."
 
+import sys
+
 from chiral.core import stats
 from chiral.net import reactor
 
@@ -9,6 +11,7 @@ from chiral.web.httpd import HTTPServer
 from chiral.shell import ChiralShellServer
 from paste.urlmap import URLMap
 from paste.httpexceptions import HTTPNotFound
+from chiral.core.threadpool import run_in_thread
 
 print "Loading applications..."
 
@@ -21,17 +24,19 @@ from chiral.web.framework import *
 print "Initializing..."
 
 @coroutine_page()
-@use_template("genshi", "chiral.web.templates.asdf")
+@use_template("genshi", "chiral.web.templates.helloworld")
 def asyncpagetest():
-	yield reactor.schedule(delay = 1)
+	yield
 	raise StopIteration({ "foo": "Test Page" })
+
+introspector = Introspector()
 
 application = URLMap()
 application.update({
 	"/pony": PonyMiddleware(HTTPNotFound()),
 	"/introspector": Introspector(),
 	"/static": StaticFileServer("/home/jacob/code/chiral/static"),
-	"/tp": asyncpagetest,
+	"/asyncpagetest": asyncpagetest,
 	"/cometlib": CometLibServer(),
 	"/": CometClock()
 })
@@ -47,11 +52,12 @@ ChiralShellServer(
 
 print "Running..."
 
-import cProfile
-
-def run():
+if "-prof" in sys.argv:
+	import cProfile
+	def run():
+		reactor.run()
+	cProfile.run(run.func_code, "test.prof")
+else:
 	reactor.run()
-
-cProfile.run(run.func_code, "test.prof")
 
 stats.dump()
