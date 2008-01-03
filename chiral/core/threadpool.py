74c16a7c9834851c97c8d25e714d08420d97a57e
jacob <jacob@a827fe29-2235-0410-a7a9-cacdfe24a5d3>
2008-01-03 22:20:27 +0000

    Thread pool: - More docs - Improve the anti-reload mechanism
    git-svn-id: http://chiral.j4cbo.com/svn/trunk@98 a827fe29-2235-0410-a7a9-cacdfe24a5d3

--- chiral/core/threadpool.py
+++ chiral/core/threadpool.py
@@ -7,6 +7,11 @@ Example::
 	@as_coro
 	def threadpool_test():
 	        yield threadpool.run_in_thread(time.sleep, 1)
+
+The thread pool is initialized automatically when the module is loaded; none of the classes
+defined here should be instantiated by the user.
+
+For implementation details, see the documentation for `ThreadPool`.
 """
 
 from __future__ import with_statement
@@ -24,20 +29,22 @@ _CHIRAL_RELOADABLE = True
 
 class ThreadPoolWatcher(tcp.TCPConnection):
 	"""
-	Helper coroutine that waits for thread pool results to come in, then dispatches them
-	to wherever they were called from.
+	Helper coroutine that dispatches thread pool results to wherever they were called from.
+
+	See `ThreadPool` for more information.
 	"""
 
-	def __init__(self, pool):
+	def __init__(self):
 		"""Constructor."""
 
 		# Prevent replacement on reload.
 		setattr(self, "__reload_update__", lambda oldobj: oldobj)
 
-		self.pool = pool
-		self.write_socket = None
+		read_socket, self.write_socket = socket.socketpair()
 
-		# Don't actually initialize until restart() is called the first time.
+		tcp.TCPConnection.__init__(self, None, read_socket)
+
+		# Don't actually start until restart() is called the first time.
 
 	def restart(self):
 		"""Restart the ThreadPoolWatcher.
@@ -47,13 +54,7 @@ class ThreadPoolWatcher(tcp.TCPConnection):
 		restart(), which will ensure that the watcher is functional.
 		"""
 
-		# At first, restart() does the actual TCP socket setup, etc. Later calls to restart()
-		# will trigger a WaitForCallback on which connection_handler is blocking.
-
-		read_socket, self.write_socket = socket.socketpair()
 		self.restart = None
-
-		tcp.TCPConnection.__init__(self, None, read_socket)
 		self.start()
 
 	def connection_handler(self):
@@ -62,16 +63,16 @@ class ThreadPoolWatcher(tcp.TCPConnection):
 			incoming = yield self.recv(128)
 			for _index in incoming:
 				# Each received byte indicates one result in the output queue.
-				result, exc, recipient = self.pool.output_queue.popleft()
+				result, exc, recipient = ThreadPool.output_queue.popleft()
 				if exc is not None:
 					recipient.throw(exc)
 				else:
 					recipient(result)
 
 			# If there are any operations still in process, continue the loop.
-			with self.pool.queue_lock:
-				ops = len(self.pool.input_queue) + self.pool.active_workers \
-				      + len(self.pool.output_queue)
+			with ThreadPool.queue_lock:
+				ops = len(ThreadPool.input_queue) + ThreadPool.active_workers \
+				      + len(ThreadPool.output_queue)
 
 			if not ops:
 				self.restart = WaitForCallback("ThreadPoolWatcher restart")
@@ -80,42 +81,62 @@ class ThreadPoolWatcher(tcp.TCPConnection):
 
 
 class ThreadPool(object):
-	"""Thread pool singleton ."""
+	"""Thread pool singleton object.
 
-	# The thread pool system has enough state that it makes sense to store
-	# it all in an object rather than a bunch of module globals. Using an
-	# object also simplifies reload handling.
+	This is essentially a dummy class; it exists as a way to store all the thread pool
+	state such that it won't be wiped out by `xreload`.
 
-	def __init__(self):
-		"""Initialize the thread pool."""
+	When `run_in_thread` is called, it creates a `WaitForCallback` which will later be
+	called with the result, and then appends the function, arguments, and callback to the
+	ThreadPool's input queue. If there are no worker threads, or more are necessary,
+	`run_in_thread` spawns a new one. It then increments ("releases") a ``threading.Semaphore``
+	associated with the input queue, and returns the `WaitForCallback` to the caller.
 
-		# queue_lock regulates access to all the queues, and must be
-		# acquired before doing any manipulation on them. The lock is
-		# held only for the duration of the manipulation.
-		self.queue_lock = threading.Lock()
+	The worker threads, when not active, block on the input queue semaphore. The semaphore
+	ensures that each time an item is added to the input queue, exactly one worker thread
+	will be woken up. It then retrieves the operation from the queue, runs it with the
+	args and kwargs that were passed to `run_in_thread`, and pushes the result onto the
+	`ThreadPool`'s output queue.
 
-		# input_queue_sem holds the number of items in the input queue.
-		# A Queue cannot be used here, because moving an operation from
-		# input_queue to the active state (active_workers) must be atomic.
-		self.input_queue = collections.deque()
-		self.input_queue_sem = threading.Semaphore(0)
+	A separate helper coroutine, the `ThreadPoolWatcher`, is responsible for returning results
+	to the main thread. The watcher is initialized from `run_in_thread` the first time the thread
+	pool is used. It creates a socketpair, and uses the `Reactor`'s socket event handling to wait on
+	the read end. Whenever a worker thread pushes a result onto the output queue, it writes a single
+	byte to the watcher's write socket. This will awaken the `ThreadPoolWatcher` coroutine in the
+	main thread, which ``recv``-s as many bytes as are available, and pops and dispatches that many
+	results from the output queue.
+	"""
+ 
+	# queue_lock regulates access to all the queues, and must be
+	# acquired before doing any manipulation on them. The lock is
+	# held only for the duration of the manipulation.
+	queue_lock = threading.Lock()
 
-		self.active_workers = 0
-		self.output_queue = collections.deque()
+	# input_queue_sem holds the number of items in the input queue.
+	# A Queue cannot be used here, because moving an operation from
+	# input_queue to the active state (active_workers) must be atomic.
+	input_queue = collections.deque()
+	input_queue_sem = threading.Semaphore(0)
 
-		self.worker_threads = {}
+	active_workers = 0
+	output_queue = collections.deque()
 
-		self.watcher = ThreadPoolWatcher(self)
+	worker_threads = {}
 
+	watcher = ThreadPoolWatcher()
 
-class ThreadPoolContainer(object):
-	pool = ThreadPool()
 	@staticmethod
 	def __reload_update__(newobj):
+		"""Prevent the class from being reinitialized by `xreload`."""
 		return newobj
 
 
 class WorkerThread(threading.Thread):
+	"""
+	Worker thread.
+
+	See `ThreadPool` for more documentation.
+	"""
 
 	__states = ( "waiting", "active", "returning" )
 
@@ -132,7 +153,7 @@ class WorkerThread(threading.Thread):
 		# The pool object itself is never replaced (though it may be modified by xreload()),
 		# so it's safe to store a reference here.
 
-		pool = ThreadPoolContainer.pool
+		pool = ThreadPool
 
 		while True:
 			# Retrieve an operation from the queue, run it, and put the result in
@@ -168,63 +189,60 @@ class WorkerThread(threading.Thread):
 		return "<WorkerThread: state %s%s>" % (self.__states[self.state], opinfo)
 
 	def _chiral_introspect(self):
+		"""Returns the introspection information for this thread."""
 		return "thread", id(self)
 
-	def introspection_info(self):
-		if self.state == 0:
-			return (self, )
-		else:
-			return (
-				self,
-				"Operation: %s" % self.operation_info[0],
-				"Args:", list(self.operation_info[1]),
-				"Kwargs: ", self.operation_info[2]
-			)
 
 @returns_waitcondition
 def run_in_thread(operation, *args, **kwargs):
 	"""Run operation in a thread, returning the result."""
 
-	pool = ThreadPoolContainer.pool
-
 	# Restart the ThreadPoolWatcher if it wasn't listening already
-	if pool.watcher.restart:
-		pool.watcher.restart()
+	if ThreadPool.watcher.restart:
+		ThreadPool.watcher.restart()
 
 	# XXX need a good algorithm for managing thread pool size
-	if not pool.worker_threads or \
-	   (pool.active_workers == len(pool.worker_threads) and pool.active_workers < 4):
+	if not ThreadPool.worker_threads or \
+	   (ThreadPool.active_workers == len(ThreadPool.worker_threads) and ThreadPool.active_workers < 4):
 		worker = WorkerThread()
 		worker.start()
-		pool.worker_threads[id(worker)] = worker
+		ThreadPool.worker_threads[id(worker)] = worker
 
 	recipient = WaitForCallback("run_in_thread %r" % (operation, ))
 
-	with pool.queue_lock:
-		pool.input_queue.append((operation, args, kwargs, recipient))
-		pool.input_queue_sem.release()
+	with ThreadPool.queue_lock:
+		ThreadPool.input_queue.append((operation, args, kwargs, recipient))
+		ThreadPool.input_queue_sem.release()
 
 	return recipient
 
 
 class _chiral_introspection(object):
+	"""Module-level introspection routines."""
 	def main(self):
 
-		pool = ThreadPoolContainer.pool
 		out = [
-			"Worker threads: %d" % len(pool.worker_threads),
-			"Input queue: %d jobs" % (len(pool.input_queue)),
+			"Worker threads: %d" % len(ThreadPool.worker_threads),
+			"Input queue: %d jobs" % (len(ThreadPool.input_queue)),
 		]
-		out.extend(pool.worker_threads.itervalues())
+		out.extend(ThreadPool.worker_threads.itervalues())
 		return out
 
 	def thread(self, thread_id):
 		try:
-			thread = ThreadPoolContainer.pool.worker_threads[int(thread_id)]
+			thread = ThreadPool.worker_threads[int(thread_id)]
 		except KeyError:
 			return None
-
-		return thread.introspection_info()
+	
+		if thread.state == 0:
+			return (thread, )
+		else:
+			return (
+				thread,
+				"Operation: %s" % thread.operation_info[0],
+				"Args:", list(thread.operation_info[1]),
+				"Kwargs: ", thread.operation_info[2]
+			)
 
 __all__ = [ "run_in_thread" ]
 
